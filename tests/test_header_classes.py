6122b8f8405d86cb54cadc8f079991cf229be8e7
Steffen Hunt <dregsboy@gmail.com>
2011-01-12 12:27:06 -0500

    Created an object to handle the parsing of the timestamp header

--- tests/test_header_classes.py
+++ tests/test_header_classes.py
@@ -1,8 +1,12 @@
 # test_header_classes.py
 
 import py
-from tnotes.headers import titleheader, timesaccessedheader, tagsheader, metadataheader
 
+from tnotes.headers import titleheader
+from tnotes.headers import timesaccessedheader 
+from tnotes.headers import tagsheader
+from tnotes.headers import metadataheader
+from tnotes.headers import timestampheader
 
 # Baseline Tests (no exceptions)
 
@@ -149,3 +153,38 @@ def test_metadata_header_correct_prefix():
     metadata_header = metadataheader.MetadataHeader()
 
     assert(metadata_header.get_prefix() == expected_output)
+
+# Timestamp Header Tests
+def test_timestamp_header_read_method():
+
+
+    timestamp_header_string = 'Timestamp: 2010-12-29 23:43:58 -0500'
+    expected_output = '2010-12-29 23:43:58 -0500'
+
+    timestamp_header = timestampheader.TimestampHeader()
+
+    timestamp_header.read(timestamp_header_string)
+
+    assert(timestamp_header.get_value() == expected_output)
+
+
+def test_timestamp_header_write_method():
+
+    
+    timestamp_header_value = '2010-12-29 23:43:58 -0500'
+    expected_output = 'Timestamp: 2010-12-29 23:43:58 -0500'
+
+    timestamp_header = timestampheader.TimestampHeader()
+    
+    timestamp_header.set_value(timestamp_header_value)
+
+    assert(timestamp_header.write() == expected_output)
+
+def test_timestamp_header_correct_prefix():
+    
+
+    expected_output = 'Timestamp'
+
+    timestamp_header = timestampheader.TimestampHeader()
+
+    assert(timestamp_header.get_prefix() == expected_output)
